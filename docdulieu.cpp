2ac35011682aeb91c64a4747451261a894511194
Cafe <ngoloihuynh2k8@gmail.com>
2026-09-23 14:12:32 +0700

    fix2

--- docdulieu.cpp
+++ docdulieu.cpp
@@ -15,7 +15,7 @@ vector<string> TVTopic(const string& tenTopic){
 	string danhsach2;
 	while (getline(file, danhsach2)){
 		if(!danhsach2.empty() && danhsach2.back() == '\r'){
-			DStu.pop_back();
+			danhsach2.pop_back();
 		}
 		if(!danhsach2.empty()){
 			DStu.push_back(danhsach2);
@@ -23,6 +23,44 @@ vector<string> TVTopic(const string& tenTopic){
 	}
 	file.close();
 	return DStu;
+}
+void nhaptay(vector<string>& wordList, const string& TuMoi){
+	if(TuMoi.empty()) return;
+	wordList.push_back(TuMoi);
+	ofstream danhsach3("words.txt", ios::app);
+	if(danhsach3.is_open()){
+		danhsach3<<TuMoi<<endl;
+		danhsach3.close();
+		cout<<TuMoi<<endl;
+	}
+	else{
+		cout<<"Mission faild"<<endl;
+	}
+}
+void UpFile(vector<string>& wordList, const string& TVfile){
+	ifstream FileTV(TVfile);
+	if(!FileTV.is_open()){
+		cout<<"Loi: UP that bai"<<TVfile<<endl;
+		return;
+	}
+	ofstream danhsach4("words.txt", ios::app);
+	string voca;
+	int sotu = 0;
+	while (getline(FileTV, voca)){
+		if(!voca.empty() && voca.back() == '\r'){
+			voca.pop_back();
+		}
+		if(!voca.empty()){
+			wordList.push_back(voca);
+			if(danhsach4.is_open()){
+				danhsach4<<voca<<endl;
+			}
+			sotu++;
+		}
+	}
+	FileTV.close();
+	danhsach4.close();
+	cout<<sotu<<TVfile<<endl;
 }
 int main(){
 	vector<string> wordList;
