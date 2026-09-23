14e618dd286dc7f83417c2aab609ca0a0c8de5a6
Cafe <ngoloihuynh2k8@gmail.com>
2026-09-23 13:21:27 +0700

    topic

--- docdulieu.cpp
+++ docdulieu.cpp
@@ -4,21 +4,41 @@
 #include <vector>
 using namespace std;
 
+vector<string> TVTopic(const string& tenTopic){
+	vector<string> DStu;
+	string CD = "topics/" + tenTopic + ".txt";
+	ifstream file(CD);
+	if (!file.is_open()){
+		cout<<"Loi: khong tim thay file du lieu!"<<tenTopic<<endl;
+		return DStu;
+	}
+	string danhsach2;
+	while (getline(file, danhsach2)){
+		if(!danhsach2.empty() && danhsach2.back() == '\r'){
+			DStu.pop_back();
+		}
+		if(!danhsach2.empty()){
+			DStu.push_back(danhsach2);
+		}
+	}
+	file.close();
+	return DStu;
+}
 int main(){
-	vector<std::string> wordList;
+	vector<string> wordList;
 	ifstream mofile("words.txt");
 	if (!mofile.is_open()) {
-		cerr << "Loi: Khong tim thay file du lieu!" <<std::endl;
+		cout<<"Loi: khong tim thay file du lieu!"<<std::endl;
 		return 0;
 	}
 	string danhsach;
-	while(std::getline(mofile, danhsach)){
+	while(getline(mofile, danhsach)){
 		if(!danhsach.empty() && danhsach.back() == '\r'){
 			danhsach.pop_back();
 		}
 		wordList.push_back(danhsach);
 	}
 	mofile.close();
-
+	cout<<wordList.size()<<endl;
 	return 0;
 }
